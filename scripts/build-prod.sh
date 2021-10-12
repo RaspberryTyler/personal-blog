c05a9270d3f946ccc8be6c3a7e1741459a23d302
Tyler Smith <tylerlwsmith@gmail.com>
2021-10-11 22:02:10 -0700

    WIP

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.build.yml build
+# The webapp must be built first because the webserver uses it as a build stage.
+docker-compose -f docker-compose.yml -f docker-compose.build.yml build webapp
+docker-compose -f docker-compose.yml -f docker-compose.build.yml build webserver
